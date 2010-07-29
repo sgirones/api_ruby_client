9757bf7fdb3a1b10377a14c738ae3186fe7d9747
David Calavera <david.calavera@gmail.com>
2010-07-29 11:02:00 +0200

    fix example

--- examples/rack_resource.rb
+++ examples/rack_resource.rb
@@ -20,18 +20,19 @@ api.datacenters.first.racks.each do |rack|
 end
 
 #
-# Delete the rack myrack01
-# 
-puts "Deleting rack 'myrack01'..."
-api.datacenters.first.racks.each do |r|
-  r.delete if r.name == 'myrack01'
+# Iterate over all the racks and print the rack name
+#
+puts "Listing racks..."
+api.datacenters.first.racks.each do |rack|
+  pp rack.name
 end
 
 #
-# Iterate over all the racks and print the rack name
-#
+# Delete the rack myrack01
+# 
 # NOT SUPPORTED IN ABIQUO1.6
-# puts "Listing racks..."
-# api.datacenters.first.racks.each do |rack|
-#   pp rack.name
-# end
+#
+#puts "Deleting rack 'myrack01'..."
+#api.datacenters.first.racks.each do |r|
+#  r.delete if r.name == 'myrack01'
+#end
