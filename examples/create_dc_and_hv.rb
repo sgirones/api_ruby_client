352b4f0bb212dcba61a8db945402930eb4c8ee97
David Calavera <david.calavera@gmail.com>
2010-09-27 12:20:21 +0200

    clean the datacenter example

--- examples/create_dc_and_hv.rb
+++ examples/create_dc_and_hv.rb
@@ -10,29 +10,29 @@ api = Abiquo::Resource('http://abiquo-server-ip:8080/api', auth)
 # Create a DataCenter
 #
 #puts "Creating DC1 in BCN..."
-api.datacenters.create :name => 'DC1', :location => 'BCN'
+new_datacenter = api.datacenters.create :name => 'DC1', :location => 'BCN'
 
 #
 # Create Remote Services
 # 
-api.datacenters.first.remoteServices.create :type => 'VIRTUAL_FACTORY', :uri => 'http://localhost:8080/virtualfactory'
-api.datacenters.first.remoteServices.create :type => 'STORAGE_SYSTEM_MONITOR', :uri => 'http://localhost:8080/ssm'
-api.datacenters.first.remoteServices.create :type => 'VIRTUAL_SYSTEM_MONITOR', :uri => 'http://localhost:8080/vsm'
-api.datacenters.first.remoteServices.create :type => 'NODE_COLLECTOR', :uri => 'http://localhost:8080/nodecollector'
-api.datacenters.first.remoteServices.create :type => 'APPLIANCE_MANAGER', :uri => 'http://localhost:8080/am'
-api.datacenters.first.remoteServices.create :type => 'DHCP_SERVICE', :uri => 'http://localhost:7911'
-api.datacenters.first.remoteServices.create :type => 'BPM_SERVICE', :uri => 'http://localhost:7911'
+new_datacenter.remoteServices.create :type => 'VIRTUAL_FACTORY', :uri => 'http://localhost:8080/virtualfactory'
+new_datacenter.remoteServices.create :type => 'STORAGE_SYSTEM_MONITOR', :uri => 'http://localhost:8080/ssm'
+new_datacenter.remoteServices.create :type => 'VIRTUAL_SYSTEM_MONITOR', :uri => 'http://localhost:8080/vsm'
+new_datacenter.remoteServices.create :type => 'NODE_COLLECTOR', :uri => 'http://localhost:8080/nodecollector'
+new_datacenter.remoteServices.create :type => 'APPLIANCE_MANAGER', :uri => 'http://localhost:8080/am'
+new_datacenter.remoteServices.create :type => 'DHCP_SERVICE', :uri => 'http://localhost:7911'
+new_datacenter.remoteServices.create :type => 'BPM_SERVICE', :uri => 'http://localhost:7911'
 
 #
 # Create a new Rack
 #
 puts "Creating rack 'myrack01'..."
-api.datacenters.first.racks.create :name => 'myrack01'
+new_datacenter.racks.create :name => 'myrack01'
 
 #
 # Iterate over all the racks and print the rack name
 #
 puts "Listing racks.."
-api.datacenters.first.racks.each do |rack|
+new_datacenter.racks.each do |rack|
   pp rack.name
 end
