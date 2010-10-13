928d85d63baa9073278d719489ccb5439d50c808
Sergio Rubio <sergio@rubio.name>
2010-10-13 19:23:07 +0200

    * updated add_hypervisor example

--- examples/add_hypervisor.rb
+++ examples/add_hypervisor.rb
@@ -4,7 +4,7 @@ require 'abiquo'
 require 'pp'
 
 auth = Abiquo::BasicAuth.new('Abiquo', 'admin', 'xabiquo')
-api = Abiquo::Resource('http://foo-server:8080/api', auth)
+api = Abiquo::Resource('http://as-testing:8080/api', auth)
 
 #
 # Create a new Rack
@@ -12,13 +12,16 @@ api = Abiquo::Resource('http://foo-server:8080/api', auth)
 dc = api.datacenters.first
 
 # Create the rack
-rack = dc.racks.create :name => 'myrack05'
+rack = dc.racks.create :name => 'myrack01'
 
 #
 # This is weird
 #
 # Create the machine
-machine = rack.machines.create :name => 'foo', :cpu => '2', :description => 'foo', :hd => '100000', :ram => '1024', :virtualSwitch => 'eth2', :state => 'STOPPED'
+machine = rack.machines.create :name => 'fooxen', :cpu => '2', :description => 'foo', :hd => '100000', :ram => '1024', :virtualSwitch => 'eth2', :state => 'STOPPED'
+
+pp machine
+
 
 # Create the HV
-hv = machine.hypervisors.create :type => 'KVM', :ip => '10.60.1.80'
+hv = machine.hypervisor.create :type => 'XEN_3', :ip => '10.0.0.1', :port => '8889', :ipService => '10.0.0.1'
